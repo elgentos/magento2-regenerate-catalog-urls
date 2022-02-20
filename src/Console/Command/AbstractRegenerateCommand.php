e8dc1f4793d2fac9f622e10546e4884bb363cb1f
Peter Jaap Blaakmeer <peterjaap@elgentos.nl>
2022-02-20 12:19:40 +0100

    Refactored method of getting storeId from input

--- src/Console/Command/AbstractRegenerateCommand.php
+++ src/Console/Command/AbstractRegenerateCommand.php
@@ -4,44 +4,96 @@ declare(strict_types=1);
 
 namespace Elgentos\RegenerateCatalogUrls\Console\Command;
 
+use Elgentos\RegenerateCatalogUrls\Service\RegenerateProductUrl;
 use Magento\Framework\App\State;
 use Magento\Framework\Exception\LocalizedException;
 use Magento\Store\Api\Data\StoreInterface;
 use Magento\Store\Model\Store;
 use Magento\Store\Model\StoreManagerInterface;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\QuestionHelper;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ChoiceQuestion;
 
 abstract class AbstractRegenerateCommand extends Command
 {
+    protected StoreManagerInterface $storeManager;
+
+    protected State $state;
+
+    protected RegenerateProductUrl $regenerateProductUrl;
+
+    protected QuestionHelper $questionHelper;
+
+    public function __construct(
+        StoreManagerInterface $storeManager,
+        State                 $state,
+        RegenerateProductUrl  $regenerateProductUrl,
+        QuestionHelper        $questionHelper
+    )
+    {
+        $this->storeManager = $storeManager;
+        $this->state = $state;
+        $this->regenerateProductUrl = $regenerateProductUrl;
+        $this->questionHelper = $questionHelper;
+        parent::__construct();
+    }
+
     protected function configure()
     {
         $this->addOption(
             'store',
             's',
-            InputOption::VALUE_REQUIRED,
-            'Regenerate for one specific store view'
+            InputOption::VALUE_OPTIONAL,
+            'Regenerate for a specific store view',
+            false
         );
     }
 
+    protected function getStoreId()
+    {
+        $storeInput = $this->input->getOption('store');
+
+        if ($storeInput === 'all') {
+            return $storeInput;
+        }
+
+        $storeId = false;
+        if (is_numeric($storeInput)) {
+            $storeId = (int) $storeInput;
+        } else if (is_string($storeInput)) {
+            $storeId = $this->getStoreIdByCode($storeInput);
+        } else if (false === $storeInput) {
+            $choices = array_merge(['all'], array_map(fn ($store) => $store->getCode(), $this->getAllStores()));
+            $question = new ChoiceQuestion(__('Pick a store')->getText(), $choices, 'all');
+            $storeCode = $this->questionHelper->ask($this->input, $this->output, $question);
+            $storeId = $this->getStoreIdByCode($storeCode);
+        }
+
+        return $storeId;
+    }
+
+    protected function getAllStores($withDefault = false): array
+    {
+        return $this->storeManager->getStores($withDefault);
+    }
 
     /**
-     * @param string $storeId
-     * @param array $stores
-     *
+     * @param string $storeCode
      * @return null|int
+     * @throws LocalizedException
      */
-    protected function getStoreIdByCode(string $storeId, array $stores): ?int
+    protected function getStoreIdByCode(string $storeCode): ?int
     {
-        foreach ($stores as $store) {
-            if ($store->getCode() === $storeId) {
+        foreach ($this->getAllStores() as $store) {
+            if ($store->getCode() === $storeCode) {
                 return (int)$store->getId();
             }
         }
 
-        return null;
+        throw new LocalizedException(__('The store that was requested (%1) wasn\'t found. Verify the store and try again.', $storeCode));
     }
 }
--- src/Console/Command/RegenerateProductUrlCommand.php
+++ src/Console/Command/RegenerateProductUrlCommand.php
@@ -7,41 +7,14 @@ namespace Elgentos\RegenerateCatalogUrls\Console\Command;
 use Elgentos\RegenerateCatalogUrls\Service\RegenerateProductUrl;
 use Magento\Framework\App\State;
 use Magento\Framework\Exception\LocalizedException;
-use Magento\Store\Api\Data\StoreInterface;
-use Magento\Store\Model\Store;
-use Magento\Store\Model\StoreManagerInterface;
-use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\QuestionHelper;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ChoiceQuestion;
 
 class RegenerateProductUrlCommand extends AbstractRegenerateCommand
 {
-    private State $state;
-
-    private StoreManagerInterface $storeManager;
-
-    private RegenerateProductUrl $regenerateProductUrl;
-
-    /**
-     * RegenerateProductUrlCommand constructor.
-     * @param State $state
-     * @param StoreManagerInterface $storeManager
-     * @param RegenerateProductUrl $regenerateProductUrl
-     */
-    public function __construct(
-        State                 $state,
-        StoreManagerInterface $storeManager,
-        RegenerateProductUrl  $regenerateProductUrl
-    )
-    {
-        $this->state = $state;
-        $this->storeManager = $storeManager;
-        $this->regenerateProductUrl = $regenerateProductUrl;
-        parent::__construct();
-    }
-
     /**
      * @return void
      */
@@ -54,6 +27,8 @@ class RegenerateProductUrlCommand extends AbstractRegenerateCommand
                 InputArgument::IS_ARRAY,
                 'Product IDs to regenerate'
             );
+
+        parent::configure();
     }
 
     /**
@@ -65,29 +40,27 @@ class RegenerateProductUrlCommand extends AbstractRegenerateCommand
      */
     public function execute(InputInterface $input, OutputInterface $output)
     {
+        $this->input = $input;
+        $this->output = $output;
+
         try {
             $this->state->getAreaCode();
         } catch (LocalizedException $e) {
             $this->state->setAreaCode('adminhtml');
         }
 
-        $stores = $this->storeManager->getStores(false);
+        $storeId = $this->getStoreId();
 
         $this->regenerateProductUrl->setOutput($output);
 
-        $storeId = $input->getOption('store');
-        if ($storeId !== 'all' && !is_null($storeId) && !is_numeric($storeId)) {
-            $storeId = $this->getStoreIdByCode($storeId, $stores);
-        }
-
-        if (!is_null($storeId) && is_numeric($storeId)) {
+        if (is_numeric($storeId)) {
             $this->regenerateProductUrl->execute($input->getArgument('pids'), $storeId, $output->isVerbose());
             return 0;
         }
 
         if ($storeId === 'all') {
-            foreach ($stores as $store) {
-                $this->regenerateProductUrl->execute($input->getArgument('pids'), $store->getId(), $output->isVerbose());
+            foreach ($this->getAllStores() as $store) {
+                $this->regenerateProductUrl->execute($input->getArgument('pids'), (int) $store->getId(), $output->isVerbose());
             }
             return 0;
         }
