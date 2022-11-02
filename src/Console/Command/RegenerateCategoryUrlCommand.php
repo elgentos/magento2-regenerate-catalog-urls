2ff8267942951ef9b9aed6947104ccb4bab7a078
Justin van Elst <vladimirgekko@gmail.com>
2022-11-02 15:16:18 +0100

    Add use statement

--- src/Console/Command/RegenerateCategoryUrlCommand.php
+++ src/Console/Command/RegenerateCategoryUrlCommand.php
@@ -15,6 +15,7 @@ use Magento\UrlRewrite\Model\UrlPersistInterface;
 use Symfony\Component\Console\Helper\QuestionHelper;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Magento\UrlRewrite\Service\V1\Data\UrlRewrite;
 use Magento\CatalogUrlRewrite\Model\CategoryUrlRewriteGenerator;
