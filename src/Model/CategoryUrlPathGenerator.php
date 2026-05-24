30f8acdabe8ed19ce0478dc094ed379b53cc20de
Lars <lars@madebymouses.com>
2026-05-24 14:26:29 +0200

    Fix incompatible parameter type hint in CategoryUrlPathGenerator override

--- src/Model/CategoryUrlPathGenerator.php
+++ src/Model/CategoryUrlPathGenerator.php
@@ -4,18 +4,16 @@ declare(strict_types=1);
 
 namespace Elgentos\RegenerateCatalogUrls\Model;
 
-use Magento\Catalog\Model\Category;
-
 class CategoryUrlPathGenerator extends \Magento\CatalogUrlRewrite\Model\CategoryUrlPathGenerator
 {
     /**
      * Determine whether generation url path for parent needed
      *
-     * @param Category $category
+     * @param \Magento\Catalog\Model\Category $category
      *
      * @return bool
      */
-    protected function isNeedToGenerateUrlPathForParent(Category $category): bool
+    protected function isNeedToGenerateUrlPathForParent($category): bool
     {
         /* Force true when command is run from the CLI */
         if (PHP_SAPI === 'cli') {
